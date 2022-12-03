252e72f3dbd1de3bb4eba747d7d92bbf22c51964
LeonardooBecker <89035881+LeonardooBecker@users.noreply.github.com>
2022-12-03 10:49:33 -0300

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,16 +1,6 @@
-from re import T
-import yfinance as yf
 import streamlit as st 
-import pandas as pd
+
 
 st.write("ALO TEMPO BAO")
 
-tickerSymbol = 'GOOGL'
 
-tickerData = yf.Ticker(tickerSymbol)
-
-tickerDf=tickerData.history(period='1d',start='2010-5-31',end='2020-5-31')
-
-st.line_chart(tickerDf.Close)
-
-st.line_chart(tickerDf.Volume)
