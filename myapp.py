32eaf7552ceefbdc54f0470085dfb5b083a1d413
lbo21 <leonardobecker79@gmail.com>
2022-12-04 09:21:36 -0300

    att

--- myapp.py
+++ myapp.py
@@ -1,11 +1,13 @@
 import streamlit as st 
-
+import pandas as pd
+import numpy as np
 
 st.write("ALO TEMPO BAO")
 st.write("teste firme")
 
 arquivo = open('Dados20220921-134049.csv','r')
 
-teste=arquivo.read()
+#teste=arquivo.read()
 
-st.write(teste)
+df = pd.DataFrame(np.random.randn(100,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
+st.map(df)
