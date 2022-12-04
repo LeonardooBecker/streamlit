6a81992eca6df8199e6417a4182d895209c019b1
lbo21 <leonardobecker79@gmail.com>
2022-12-04 09:23:48 -0300

    att

--- myapp.py
+++ myapp.py
@@ -9,5 +9,5 @@ arquivo = open('Dados20220921-134049.csv','r')
 
 #teste=arquivo.read()
 
-df = pd.DataFrame(np.random.randn(100,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
+df = pd.DataFrame(np.random.randn(100,2)/[50,50]+[37.76,-122.4],columns=[-49.269366,-25.436129])
 st.map(df)
