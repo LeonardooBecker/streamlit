767c3dd41677cbd8e9c17d330c86a64835174831
lbo21 <leonardobecker79@gmail.com>
2022-12-05 20:40:00 -0300

    n

--- myapp.py
+++ myapp.py
@@ -3,9 +3,19 @@ import folium
 import pandas as pd
 from streamlit_folium import folium_static
 
+st.sidebar.header("user input parameters")
+info=st.sidebar.selectbox('viagem',('Primeira','Segunda'))
+
+
+
+
 my_map = folium.Map(location=[-25.436085,-49.269290],zoom_start=13)
 
-arquivo = open('Dados20220921-190631.csv', 'r')
+
+if(info=='Primeira'):
+    arquivo = open('Dados20220921-190631.csv', 'r')
+if(info=='Segunda'):
+    arquivo = open('Dados20220921-134049.csv', 'r')
 total = arquivo.readlines()
 
 init = total[1].split(",")
