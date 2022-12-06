4cc1f3b3059b8d77bbcdf4fb372ff7f496bf813b
lbo21 <leonardobecker79@gmail.com>
2022-12-06 19:22:15 -0300

    new

--- myapp.py
+++ myapp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import folium
+import re
 import pandas as pd
 from streamlit_folium import folium_static
 
@@ -15,19 +16,30 @@ my_map = folium.Map(location=[-25.436085,-49.269290],zoom_start=13)
 if(info=='Primeira'):
     arquivo = open('Dados20220921-190631.csv', 'r')
 if(info=='Segunda'):
-    arquivo = open('Fulltable_20220429_EFGHSTUV.xlsx', 'r')
+    arquivo = open('Fulltable_20220429_EFGHSTUV.csv', 'r')
+
 total = arquivo.readlines()
 
-init = total[1].split(",")
 
 ignoraPrimeira = 0
 
-for linha in total:
+for linhaAtual in total:
     if (ignoraPrimeira == 0):
         ignoraPrimeira += 1
     else:
-        linha = linha.split(",")
-        folium.Circle([(float(linha[2])),(float(linha[1]))],5,color='black',fill=True,fill_color='black',fill_opacity=1).add_to(my_map)
+        linha = re.split('[,;]',linhaAtual)
+        data=linha[5]
+        data = data.split('/')
+        if(len(data)>1 and float(data[2])==2021):
+            longitude = linha[1] + '.' + linha[2]
+            longitude = float(longitude)
+            latitude = linha[3] + '.'  + linha[4]
+            latitude = float(latitude)
+        else:
+            longitude=float(linha[1])
+            latitude=float(linha[2])
+            
+        folium.Circle([latitude,longitude],5,color='black',fill=True,fill_color='black',fill_opacity=1).add_to(my_map)
 
 
 folium_static(my_map,width=800,height=475)
